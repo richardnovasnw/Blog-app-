fde9f0dd7477d7f39e9efb06bcf4a188b0399cf8
richardnovasnw <88481980+richardnovasnw@users.noreply.github.com>
2021-09-21 17:52:12 +0530

    Delete lib directory

--- lib/provider/app_state_notifier.dart
+++
@@ -1,9 +0,0 @@
-import 'package:state_notifier/state_notifier.dart';
-
-abstract class AppStateNotifier<T> extends StateNotifier<T> {
-  AppStateNotifier(T state) : super(state);
-
-  void init() {
-    print('initializing');
-  }
-}
